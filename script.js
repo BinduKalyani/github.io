93d40ae96c2b192fca47cdf4f570d63d66dd04d3
Guntupalli Bindu Kalyani <69250764+BinduKalyani@users.noreply.github.com>
2026-03-26 15:24:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,4 +9,22 @@ function loadMarkdown(file, elementId) {
 loadMarkdown("about.md", "about");
 loadMarkdown("interests.md", "interests");
 loadMarkdown("experience.md", "experience");
-loadMarkdown("projects.md", "projects");
+
+fetch('data/projects.json')
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById('project-list');
+
+    data.projects.forEach(p => {
+      const div = document.createElement('div');
+      div.className = "project-item";
+
+      div.innerHTML = `
+        <strong>${p.title}</strong><br>
+        <a href="${p.link}" target="_blank">View</a>
+      `;
+
+      container.appendChild(div);
+    });
+  });
+
